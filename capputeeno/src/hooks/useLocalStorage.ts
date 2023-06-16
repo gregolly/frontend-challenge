3181c36cfba1e726d0990cd5803f01368dfd38d5
gregolly <gregollyff@hotmail.com>
2023-06-16 16:39:24 -0400

    feat: trying a new approach about localstorage

--- capputeeno/src/hooks/useLocalStorage.ts
+++ capputeeno/src/hooks/useLocalStorage.ts
@@ -1,23 +1,34 @@
 import { useEffect, useState } from "react";
 
-export function useLocalStorage<T>(item: string, initialValue: T) {
-    const [value, setValue] = useState<T>(initialValue)
+export function useLocalStorage<T>(key: string, initialValue: T) {
+    const [value, setValue] = useState(() => {
+        const storedValue = localStorage.getItem(key);
+        return storedValue ? JSON.parse(storedValue) : initialValue;
+      });
+    
+      useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(value));
+      }, [key, value]);
+    
+      return{ value, setValue };
+      
+    // const [value, setValue] = useState<T>(initialValue)
 
-    const varWindow = window
+    // const varWindow = window
 
-    useEffect(() => {
-        if (typeof varWindow === 'undefined') return;
-        let value = localStorage.getItem(item)
-        if (value) setValue(JSON.parse(value))
-    }, [varWindow])
+    // useEffect(() => {
+    //     if (typeof varWindow === 'undefined') return;
+    //     let storedValue = localStorage.getItem(key)
+    //     if (storedValue) setValue(JSON.parse(storedValue))
+    // }, [varWindow])
 
-    function updateLocalStorage(newValue: T) {
-        setValue(newValue)
-        localStorage.setItem(item, JSON.stringify(newValue))
-    }
+    // function updateLocalStorage(newValue: T) {
+    //     setValue(newValue)
+    //     localStorage.setItem(key, JSON.stringify(newValue))
+    // }
 
-    return {
-        value,
-        updateLocalStorage,
-    }
+    // return {
+    //     value,
+    //     updateLocalStorage,
+    // }
 }
