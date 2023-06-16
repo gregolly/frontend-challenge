66d467bb020896726deec168e36a5962b8dcc039
gregolly <gregollyff@hotmail.com>
2023-06-16 14:44:57 -0400

    fix: add cache time to requests

--- capputeeno/src/hooks/useLocalStorage.ts
+++ capputeeno/src/hooks/useLocalStorage.ts
@@ -3,11 +3,13 @@ import { useEffect, useState } from "react";
 export function useLocalStorage<T>(item: string, initialValue: T) {
     const [value, setValue] = useState<T>(initialValue)
 
+    const varWindow = window
+
     useEffect(() => {
-        if (typeof window === 'undefined') return;
+        if (typeof varWindow === 'undefined') return;
         let value = localStorage.getItem(item)
         if (value) setValue(JSON.parse(value))
-    })
+    }, [varWindow])
 
     function updateLocalStorage(newValue: T) {
         setValue(newValue)
