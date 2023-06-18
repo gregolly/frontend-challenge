51957019a1688628217310966aeda2f6baed33e5
gregolly <gregollyff@hotmail.com>
2023-06-18 09:51:06 -0400

    refactor: localstorage hook

--- capputeeno/src/hooks/useLocalStorage.ts
+++ capputeeno/src/hooks/useLocalStorage.ts
@@ -1,17 +1,22 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorage<T>(key: string, initialValue: T) {
-    const [value, setValue] = useState(() => {
-        const storedValue = localStorage.getItem(key);
-        return storedValue ? JSON.parse(storedValue) : initialValue;
+    const [value, setValue] = useState<T>(() => {
+        const storedValue = localStorage.getItem(key)
+        return storedValue ? JSON.parse(storedValue) : initialValue
       });
     
       useEffect(() => {
-        localStorage.setItem(key, JSON.stringify(value));
-      }, [key, value]);
+        localStorage.setItem(key, JSON.stringify(value))
+      }, [key, value])
+
+      function updateLocalStorage(newValue: T) {
+        setValue(newValue)
+        localStorage.setItem(key, JSON.stringify(newValue))
+      }
     
-      return{ value, setValue };
-      
+      return { value, setValue, updateLocalStorage }
+
     // const [value, setValue] = useState<T>(initialValue)
 
     // const varWindow = window
