c31760418b4f5f319adc288f47b82030941a36fc
gregolly <gregollyff@hotmail.com>
2023-06-18 09:46:05 -0400

    feat: add new colors

--- capputeeno/src/styles/themes/default.ts
+++ capputeeno/src/styles/themes/default.ts
@@ -3,6 +3,7 @@ export const defaultTheme = {
         white: '#fff',
         alert: '#DE3838',
         text: '#617480',
+        others: '#51B853',
         'bg-secondary': "#F3F5F6",
         'text-dark': "#737380",
         'orange-low': '#FFA585',
@@ -10,7 +11,8 @@ export const defaultTheme = {
         'text-dark-2': '#41414D',
         'shapes-dark': '#09090A',
         'shapes-2': '#DCE2E6',
-        'shapes-light': '#F0F0F5'
+        'shapes-light': '#F0F0F5',
+        'border-color': '#A8A8B3'
     },
     MediaQueryList: {
         desktop: '1024px',
